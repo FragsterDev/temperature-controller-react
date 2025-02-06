5f48a8cada34a69e8ac62a9fb76ba10f1b02d42b
Pratik059 <72210275+Pratik059@users.noreply.github.com>
2025-02-06 21:47:50 +0530

    made functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
+import { useState } from 'react'
 import Board from './components/board'
 
-
 function App() {
+
+  const [temp, setTemp] = useState(0)
+  const [bg, setBg] = useState('rgb(3,90,166)')
+
+  function increaseTemp(){
+    if(temp === 30)
+      return
+
+    const newTemp = temp + 1
+
+    if(newTemp>=17)
+      setBg('rgb(255,82,0)')
+
+    setTemp(newTemp)
+  }
+
+  function decreaseTemp(){
+
+    if(temp === 0)
+      return
+
+    const newTemp = temp - 1
+
+    if(newTemp<17)
+      setBg("rgb(3,90,166)")
+
+    setTemp(newTemp)
+  }
+
   return (
     <div className={"main mt-4 flex justify-center"}>
-      <Board />
+      <Board temp={temp} setTemp={setTemp} increaseTemp={increaseTemp} decreaseTemp={decreaseTemp} bg={bg}/>
     </div>
   )
 }
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,21 +1,26 @@
-function Display(){
+function Display({ temp, bg }){
+  
   return (
-    <div className={"w-52 h-52 bg-[rgb(3,90,166)] rounded-full border-3 border-amber-50 items-center flex justify-center"}>
-      <div className={"temp text-amber-50 text-4xl font-medium"}>30 C</div>
+    <div className={`w-52 h-52 rounded-full border-3 border-amber-50 items-center flex justify-center`} style={{ backgroundColor: bg }}>
+      <div className={"temp text-amber-50 text-4xl font-medium"}>{temp} °C</div>
     </div>
   )
 }
 
-function Buttons(){
+function Buttons({ increaseTemp, decreaseTemp}){
   return (
     <div className="w-64 h-24 flex justify-around">
-      <button className="">
+      <button className="increase" onClick={() => {
+        increaseTemp()
+      }}>
         <div className="w-20 h-20 rounded-full bg-[rgb(105,104,104)] hover:bg-[rgb(184,184,184)] hover:cursor-pointer border-2 border-amber-50  flex justify-center items-center">
           <p className="text-white text-3xl">+</p>
         </div>
       </button>
 
-      <button className="">
+      <button className="decrease" onClick={() => {
+        decreaseTemp()
+      }}>
       <div className="w-20 h-20 rounded-full bg-[rgb(105,104,104)] border-2 border-amber-50 hover:bg-[rgb(184,184,184)] cursor-pointer flex justify-center items-center">
         <p className="text-white text-3xl">-</p>
       </div>
@@ -25,11 +30,12 @@ function Buttons(){
 }
 
 
-function Board() {
+function Board(props) {
+  const { temp, setTemp, increaseTemp, decreaseTemp, bg} = props
   return (
-    <div className={"font-bold w-72 h-96 bg-[rgb(43,89,112)] rounded-2xl justify-around flex flex-col items-center"}>
-      <Display />
-      <Buttons />
+    <div className={"font-bold w-72 h-96 bg-[rgb(43,89,112)] rounded-2xl justify-around flex flex-col items-center shadow-2xl shadow-gray-800"}>
+      <Display temp={temp} bg={bg}/>
+      <Buttons setTemp={setTemp} increaseTemp={increaseTemp} decreaseTemp={decreaseTemp} bg={bg} temp={temp}/>
     </div>
   )
 }
